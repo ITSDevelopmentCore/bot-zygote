64b80df420638e846e2aa299b4bb7e0f8d831009
ITS_Razex <itsdevelopmentru@gmail.com>
2022-10-23 04:31:17 +0500

    CHANGE ENVIRONMENT TO PRODUCTION

--- src/main/java/its/development/wrapper/EnvironmentConstants.java
+++ src/main/java/its/development/wrapper/EnvironmentConstants.java
@@ -1,10 +1,11 @@
 package its.development.wrapper;
 
 import static its.development.wrapper.EnvironmentConstants.BuildType.DEBUG;
+import static its.development.wrapper.EnvironmentConstants.BuildType.PRODUCTION;
 
 public class EnvironmentConstants {
 
-    public static final BuildType BUILD_TYPE = DEBUG;
+    public static final BuildType BUILD_TYPE = PRODUCTION;
 
     public enum BuildType {
         PRODUCTION, DEBUG
